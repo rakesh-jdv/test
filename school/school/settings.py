7af37d83393ff57e6cd10ee83508cee3b4cac8a1
rakesh-jdv <82428224+rakesh-jdv@users.noreply.github.com>
2021-06-06 21:58:05 +0530

    Add files via upload

--- school/school/settings.py
+++ school/school/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8659l39lmccsy#itk%9st7xq9577z+3-j1+xo^fn)hwartzdy+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1:8000','mya1.herokuapp.com']
 
 
 # Application definition
